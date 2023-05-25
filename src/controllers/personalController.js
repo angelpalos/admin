3978ff03dd236e1e962a26c9690238f8dc7eaaea
Angie Palos <saulblue30@gmail.com>
2023-05-25 02:55:32 -0600

    Identacion

--- src/controllers/personalController.js
+++ src/controllers/personalController.js
@@ -18,80 +18,79 @@ function index(req, res) {
   }
   
 //Redirige a la pagina para crear un nuevo usuario
-  function create(req, res) {
-  
-    res.render('pages/create');
-  }
+function create(req, res) {
+
+  res.render('pages/create');
+}
   
 //La fuincion tiene el trabajo de verificar si el usuario (email) se repite (Con el querie) entra a una condicion
 //Que si es asi mandara un error al Usuario que dira 'El usuario ya existe'
 //Si no es asi, la password se encriptara con la costante de 'bcrypt'
 //Y toda la informacion del formulario se insertara a la base de datos (con el querie) 
-  function store(req, res) {
-    const data=req.body;
-    req.getConnection((err,conn) => {
-      conn.query('SELECT * FROM users WHERE email= ?',[data.email], (err,userData) => {
-        if (userData.length>0){
-          res.render('pages/create', {error: 'El usuario ya existe!'});
-        } else if (userData.length<0){
-          bcrypt.hash(data.password, 12).then(hash => {
-            console.log(hash);
-            data.password=hash;
-            //console.log(data);
-            req.getConnection((err,conn) => {
-                conn.query('INSERT INTO users SET ?',[data], (err,rows) => {
-                  res.redirect('/pers'); 
-                });
-            });
-        
+function store(req, res) {
+  const data=req.body;
+  req.getConnection((err,conn) => {
+    conn.query('SELECT * FROM users WHERE email= ?',[data.email], (err,userData) => {
+      if (userData.length>0){
+        res.render('pages/create', {error: 'El usuario ya existe!'});
+      } else if (userData.length<=0){
+        bcrypt.hash(data.password, 12).then(hash => {
+          console.log(hash);
+          data.password=hash;
+          //console.log(data);
+          req.getConnection((err,conn) => {
+              conn.query('INSERT INTO users SET ?',[data], (err,rows) => {
+                res.redirect('/pers'); 
+              });
           });
-        }
-      });
+      
+        });
+      }
     });
+  });
 }
   
 
 //Borra la informacion seleccionada de la base de datos
 //Al ejecutar eso te redirige a la pagina de "/pers"
-  function destroy(req, res) {
-    const id_usuario = req.body.id;
-    
-    req.getConnection((err, conn) => {
-      conn.query('DELETE FROM users WHERE id_usuario = ?', [id_usuario], (err, rows) => {
-        res.redirect('/pers');
-       });
-   })
- 
- }
+function destroy(req, res) {
+  const id_usuario = req.body.id;
+  
+  req.getConnection((err, conn) => {
+    conn.query('DELETE FROM users WHERE id_usuario = ?', [id_usuario], (err, rows) => {
+      res.redirect('/pers');
+      });
+  })
+}
   
 
  //Selecciona de la tabla "Users" toda la informacion
  //Al ejecitarlo te redirige a la pagina para editar al usuario
-  function edit(req, res) {
-    const id_usuario = req.params.id;
+function edit(req, res) {
+  const id_usuario = req.params.id;
 
-    req.getConnection((err, conn) => {
-     conn.query('SELECT * FROM users WHERE id_usuario = ?', [id_usuario], (err, pers) => {
-        if(err) {
-          res.json(err);
-        }
-        res.render('pages/edit', { pers });
-      });
+  req.getConnection((err, conn) => {
+    conn.query('SELECT * FROM users WHERE id_usuario = ?', [id_usuario], (err, pers) => {
+      if(err) {
+        res.json(err);
+      }
+      res.render('pages/edit', { pers });
     });
-  }
+  });
+}
   
 
 //Actualiza los datos de la tabla user en la base de datos (querie)
 //vuelve a encriptar la contraseña con el "bcrypt"
 //Redirige a la pagina de "/pers"
-  function update(req, res) {
-    const id_usuario = req.params.id;
-    const data = req.body;
+function update(req, res) {
+  const id_usuario = req.params.id;
+  const data = req.body;
 
-    bcrypt.hash(data.password, 12).then(hash => {
-      console.log(hash);
-      data.password=hash;
-  
+  bcrypt.hash(data.password, 12).then(hash => {
+    console.log(hash);
+    data.password=hash;
+
     req.getConnection((err, conn) => {
       conn.query('UPDATE users SET ? WHERE id_usuario = ?', [data, id_usuario], (err, rows) => {
         if(err) {
@@ -100,33 +99,33 @@ function index(req, res) {
         res.redirect('/pers');
       });
     });
-   });
-  }
+  });
+}
 
-  function buscar(req, res){
-    const data = req.body;
-    const busc =  data.buscador + '%'
-    console.log(busc)
-    req.getConnection((err, conn)=>{
-      conn.query("SELECT * FROM users WHERE name LIKE ? ORDER BY `name` ASC ", [busc], (err, pers) => {
-        console.log(data.buscador)
-        console.log(err)
-        console.log(pers)
-        res.render('pages/personal', {pers})
-        
-      });
+function buscar(req, res){
+  const data = req.body;
+  const busc =  data.buscador + '%'
+  console.log(busc)
+  req.getConnection((err, conn)=>{
+    conn.query("SELECT * FROM users WHERE name LIKE ? ORDER BY `name` ASC ", [busc], (err, pers) => {
+      console.log(data.buscador)
+      console.log(err)
+      console.log(pers)
+      res.render('pages/personal', {pers})
+      
     });
-  }
+  });
+}
 
   
   
 //exporta las funciones 
-  module.exports = {
-    index: index,
-    create: create,
-    store: store,
-    destroy: destroy,
-    edit: edit,
-    update: update,
-    buscar,
-  }
+module.exports = {
+  index: index,
+  create: create,
+  store: store,
+  destroy: destroy,
+  edit: edit,
+  update: update,
+  buscar,
+}
